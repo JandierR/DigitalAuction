2ed6d2a61f4f7c1d5d0c7741f2d5b0d4036494ac
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-06-24 15:21:04 -0600

    Initial commit

---
+++ src/test/java/myauction/digitalauction/DigitalAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package myauction.digitalauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
