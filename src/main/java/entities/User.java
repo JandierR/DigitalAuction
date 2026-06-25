8d293be5d622503ef6d06f7fc9ccd695269d4305
MoonLord-1 <paulovargas052@gmail.com>
2026-06-25 13:50:24 -0600

    test commit

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -17,13 +17,22 @@ public class User {
     @Id
     private UUID id;
 
+
+
+    //@NotBlank(message= "Necesitas digitar un nombre")
+    //@Size(min= 3, max= 20)
     private String name;
 
+    //@NotBlank(message= "Necesitas digitar un usuario")
+    //@Size(min= 3, max= 20)
     private String nickname;
 
 
+   //@NotBlank(message= "!Necesitas una contraseña¡")
+    //@Size(min= 8, max= 20)
     private String password;
 
+    //@PositiveOrZero
     private double balance;
 
 }
